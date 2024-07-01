89be12c75a186931a820440b6ecd8030fed8febd
McAr0v <dvijkz@gmail.com>
2024-07-01 17:42:47 +0500

    Почистил класс пользователя

--- lib/current_user/user_class.dart
+++ lib/current_user/user_class.dart
@@ -26,16 +26,9 @@ class UserCustom with MixinDatabase, UserAuthMixin, UsersListsMixin, FavListsMix
   Genders gender;
   String avatar;
   DateTime registrationDate;
-  //List<String> myEvents;
   List<MyPlaces> myPlaces;
-  //List<String> myPromos;
-  //List<String> favPlaces;
-  //List<String> favEvents;
-  //List<String> favPromos;
-
 
   static UserCustom? currentUser;
-
 
   UserCustom({
     required this.uid,
@@ -52,12 +45,7 @@ class UserCustom with MixinDatabase, UserAuthMixin, UsersListsMixin, FavListsMix
     required this.gender,
     required this.avatar,
     required this.registrationDate,
-    //required this.myEvents,
     required this.myPlaces,
-    //required this.myPromos,
-    //required this.favEvents,
-    //required this.favPlaces,
-    //required this.favPromos,
   });
 
   factory UserCustom.empty() {
@@ -76,12 +64,7 @@ class UserCustom with MixinDatabase, UserAuthMixin, UsersListsMixin, FavListsMix
       gender: Genders(),
       avatar: AppConstants.defaultAvatar,
       registrationDate: DateTime.now(),
-      //myEvents: [],
       myPlaces: [],
-      //myPromos: [],
-      //favEvents: [],
-      //favPlaces: [],
-      //favPromos: []
     );
   }
 
@@ -99,15 +82,8 @@ class UserCustom with MixinDatabase, UserAuthMixin, UsersListsMixin, FavListsMix
     AppRole appRole = AppRole();
     appRole.setRoleFromDb(infoSnapshot.child(AppConstants.userRoleProperty).value.toString());
 
-    //List<String> favPromos = FavListsMixin.getFavEntitiesId(snapshot.child(AppConstants.favPromoPathKey), AppConstants.favPromoIdKey);
-    //List<String> favPlaces = FavListsMixin.getFavEntitiesId(snapshot.child(AppConstants.favPlacePathKey), AppConstants.favPlaceIdKey);
-    //List<String> favEvents = FavListsMixin.getFavEntitiesId(snapshot.child(AppConstants.favEventPathKey), AppConstants.favEventIdKey);
-
     List<MyPlaces> myPlaces = FavListsMixin.getMyPlacesEntities(snapshot.child(AppConstants.myPlacePathKey));
 
-
-    //List<String> myPromos = FavListsMixin.getFavEntitiesId(snapshot.child(AppConstants.myPromoPathKey), AppConstants.favPromoIdKey);
-    //List<String> myEvents = FavListsMixin.getFavEntitiesId(snapshot.child(AppConstants.myEventPathKey), AppConstants.favEventIdKey);
 
     // Берем из них данные и заполняем в класс Gender И возвращаем его
     return UserCustom(
@@ -125,12 +101,7 @@ class UserCustom with MixinDatabase, UserAuthMixin, UsersListsMixin, FavListsMix
       gender: gender,
       avatar: infoSnapshot.child(AppConstants.userAvatarProperty).value.toString(),
       registrationDate: DateMixin.getDateFromString(infoSnapshot.child(AppConstants.userRegistrationDateProperty).value.toString()),
-      //myEvents: myEvents,
-      //myPromos: myPromos,
       myPlaces: myPlaces,
-      //favPromos: favPromos,
-      //favPlaces: favPlaces,
-      //favEvents: favEvents
     );
   }
 
@@ -153,7 +124,6 @@ class UserCustom with MixinDatabase, UserAuthMixin, UsersListsMixin, FavListsMix
   // Метод для обновления данных текущего пользователя
   static void updateCurrentUser(UserCustom updatedUser) {
     currentUser = updatedUser;
-    //updateAccessLevel(updatedUser.role);
   }
 
   Map<String, dynamic> generateEntityDataCode(){
@@ -185,58 +155,11 @@ class UserCustom with MixinDatabase, UserAuthMixin, UsersListsMixin, FavListsMix
 
     if (notAdminChanges)
     {
-      currentUser = this; // Обновляем текущего пользователя
-      //updateAccessLevel(user.role);
+      currentUser = this;
     }
 
     return result;
   }
-
-  /*void deleteEventFromFav(String id){
-    if (favEvents.contains(id)){
-      favEvents.removeWhere((event) => event == id);
-    }
-
-  }*/
-
-
-
-  /*void addEventToFav(String id){
-    if (!favEvents.contains(id)){
-      favEvents.add(id);
-    }
-
-  }*/
-
-  /*void deleteEventFromMy(String id){
-    if (myEvents.contains(id)){
-      myEvents.removeWhere((event) => event == id);
-    }
-
-  }*/
-
-  /*void addEventToMy(String id){
-    if (!myEvents.contains(id)){
-      myEvents.add(id);
-    }
-
-  }*/
-
-  /*void deletePlaceFromFav(String id){
-    if (favPlaces.contains(id)){
-      favPlaces.removeWhere((place) => place == id);
-    }
-
-  }*/
-
-
-
-  /*void addPlaceToFav(String id){
-    if (!favPlaces.contains(id)){
-      favPlaces.add(id);
-    }
-
-  }*/
 
   PlaceUserRole getPlaceRoleFromMyPlaces(String placeId){
 
@@ -271,7 +194,6 @@ class UserCustom with MixinDatabase, UserAuthMixin, UsersListsMixin, FavListsMix
     }
 
   }
-  //.removeWhere((event) => event.id == entityId);
 
   // ---- ФУНКЦИЯ ЧТЕНИЯ ИНФОРМАЦИИ О ПОЛЬЗОВАТЕЛЕ -----
 
@@ -286,8 +208,7 @@ class UserCustom with MixinDatabase, UserAuthMixin, UsersListsMixin, FavListsMix
     if (snapshot != null) {
       user = UserCustom.fromSnapshot(snapshot);
       if (!onlyRead){
-        //updateAccessLevel(user.role);
-        currentUser = user; // Обновляем текущего пользователя
+        currentUser = user;
       }
     }
     return user;
